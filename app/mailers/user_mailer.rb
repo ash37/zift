3a582b8e83529b9d44aa9beeac4e63e08d3012f9
Ashley Knight <ak@subcq.com>
2025-08-11 22:17:29 +1000

    Employee file creation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def invitation_email
     @user = params[:user]
     @onboarding_url = accept_user_invitation_url(invitation_token: @user.invitation_token)
-    mail(to: @user.email, subject: "You have been invited to join Qcare")
+    mail(to: @user.email, subject: "Qcare employment successful")
   end
 end
