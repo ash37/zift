c9e3381936505c5b65c0f27ad3080860e9b4c6fe
Ashley Knight <ak@subcq.com>
2025-08-24 07:50:44 +1000

    Style mailers

--- app/mailers/roster_mailer.rb
+++ app/mailers/roster_mailer.rb
@@ -6,6 +6,6 @@ class RosterMailer < ApplicationMailer
     @roster = params[:roster]
     @shifts = @roster.shifts.where(user: @user).order(:start_time)
 
-    mail(to: @user.email, subject: "Your roster for the week of #{@roster.starts_on.strftime('%B %d')}")
+    mail(to: @user.email, subject: "Your Qcare shifts for the week starting #{@roster.starts_on.strftime('%B %d')}")
   end
 end
