765053c23a51459887a0675b9f4d971400a2abc7
Ashley Knight <ak@subcq.com>
2025-08-25 17:44:41 +1000

    Set default_url_options host and force SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,13 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 
+  Rails.application.routes.default_url_options[:host] =
+    ENV.fetch("APP_HOST", "fathomless-lowlands-58156.herokuapp.com")
+
+  config.action_controller.default_url_options = {
+    host: ENV.fetch("APP_HOST", "fathomless-lowlands-58156.herokuapp.com")
+  }
+
   # Skip http-to-https redirect for the default health check endpoint.
   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
