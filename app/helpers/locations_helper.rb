4a2fa8e45e7b3d503638a9b88f123cb6bdcd5f7c
Ashley Knight <ak@subcq.com>
2025-09-29 04:16:06 +1000

    Conditional icon for clients index

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,29 @@
 module LocationsHelper
+  def location_compliance_status(location, current_service_agreement = nil)
+    missing = []
+
+    begin
+      signed = if current_service_agreement.present?
+                 LocationAgreementAcceptance.where(location: location, agreement: current_service_agreement)
+                                             .where.not(signed_at: nil)
+                                             .exists?
+               else
+                 LocationAgreementAcceptance.joins(:agreement)
+                                             .where(location: location, agreements: { document_type: 'service' })
+                                             .where.not(signed_at: nil)
+                                             .exists?
+               end
+      missing << 'Service agreement not signed' unless signed
+
+      support_plan_ok = location.support_plan.attached?
+      missing << 'Support plan missing' unless support_plan_ok
+
+      risk_assessment_ok = location.risk_assessment.attached?
+      missing << 'Risk assessment missing' unless risk_assessment_ok
+
+      [missing.empty?, missing]
+    rescue StandardError
+      [false, ['Status unavailable']]
+    end
+  end
 end
