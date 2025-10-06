514a314ea29233b0a82011875c6406402339363a
Ashley Knight <ak@subcq.com>
2025-10-06 15:37:29 +1000

    Fix migration

--- db/migrate/20251004123000_remove_unused_columns_from_incidents.rb
+++ db/migrate/20251004123000_remove_unused_columns_from_incidents.rb
@@ -1,10 +1,15 @@
 class RemoveUnusedColumnsFromIncidents < ActiveRecord::Migration[7.1]
-  def change
-    change_table :incidents, bulk: true do |t|
-      t.remove :incident_country
-      t.remove :incident_address_line2
-      t.remove :reported_to
-      t.remove :other_clients
-    end
+  def up
+    remove_column :incidents, :incident_country if column_exists?(:incidents, :incident_country)
+    remove_column :incidents, :incident_address_line2 if column_exists?(:incidents, :incident_address_line2)
+    remove_column :incidents, :reported_to if column_exists?(:incidents, :reported_to)
+    remove_column :incidents, :other_clients if column_exists?(:incidents, :other_clients)
+  end
+
+  def down
+    add_column :incidents, :incident_country, :string unless column_exists?(:incidents, :incident_country)
+    add_column :incidents, :incident_address_line2, :string unless column_exists?(:incidents, :incident_address_line2)
+    add_column :incidents, :reported_to, :text unless column_exists?(:incidents, :reported_to)
+    add_column :incidents, :other_clients, :text unless column_exists?(:incidents, :other_clients)
   end
 end
